285187850c5f80116fcae929ac7c2c0f114e3301
A01793650 <113482378+A01793650@users.noreply.github.com>
2024-06-16 20:49:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ st.title('Análisis de Rutas - Fuente: SATRACK')
 
 # Instrucciones para el usuario
 st.write('En esta aplicación podrás:')
-st.write('1. Identifiar rápidamente lo lugares donde uno o varios vehículo paran y por cuanto tiempo lo hicieron')
-st.write('2.Extraer el archivo identificando si esta en puntos autorizados')
+st.write('1. Identificar rápidamente los lugares donde uno o varios vehículos paran y por cuanto tiempo lo hicieron')
+st.write('2. Extraer el archivo identificando si estuvo o estuvieron en puntos autorizados')
 st.write(' ')         
 st.write('Cargue el archivo de Detalle del Recorrido.')
 st.write('')
