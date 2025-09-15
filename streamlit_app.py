a9a565be6d235d627229de208248de74395943af
A01793650 <113482378+A01793650@users.noreply.github.com>
2025-09-15 16:13:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,5 +77,3 @@ if os.path.isfile(archivo_html):
     if st.button('Descargar Mapa'):
         contenido_archivo = descargar_html()
         st.download_button(label='Haz clic para descargar', data=contenido_archivo, file_name='Mapa_Analisis.html', mime='text/html')
-    else:
-        st.error('El archivo HTML generado no se encontró. Por favor, genera el mapa primero.')
