81b3a74ae4017bc948be986e44d4597ebf316607
A01793650 <113482378+A01793650@users.noreply.github.com>
2025-09-15 15:59:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ from rtree import index
 
 # Lista de GeoJSON desde GitHub (raw links)
 geojson_urls = [
-    "https://raw.githubusercontent.com/A01793650/satrack/main/break.geojson"
+    "https://raw.githubusercontent.com/A01793650/satrack/main/break.geojson",
+    "https://raw.githubusercontent.com/A01793650/satrack/blob/main/bus.geojson"
 ]
 st.title("Visor de GeoJSON desde GitHub 🌍")
 
